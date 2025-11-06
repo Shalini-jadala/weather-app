e5d8c7887d027ae1a3a58ac3d4998321ed57159d
Shalini-jadala <jspforever143@gmail.com>
2025-11-06 19:22:53 +1100

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 function getWeather(response){
     let temperatureElement = document.querySelector("#city-temperature");
     let cityTemperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+
+
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    humidityElement.innerHTML =  `${response.data.temperature.humidity}%,`;
     temperatureElement.innerHTML = Math.round(cityTemperature);
+    
    
 }
+   function formatDate(date){
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
 
-function searchCity(city) {
+
+     let day = days[date.getDay()];
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+
+
+    return `${day} ${hours}:${minutes}`;
+
+   }
+
+
+    function searchCity(city) {
     let apiKey = "0a8c6e7ct4a98df4436abe8fo0c14f17";
     let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(getWeather);
@@ -18,10 +50,10 @@ function searchCity(city) {
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = searchInput.value;
     searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("India");
 
