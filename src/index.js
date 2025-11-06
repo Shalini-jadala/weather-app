69ee3f8357ca3442938eb35f52038d57e04e2e14
Shalini-jadala <jspforever143@gmail.com>
2025-11-06 17:15:06 +1100

    added search function by using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function getWeather(response){
+    let temperatureElement = document.querySelector("#city-temperature");
+    let cityTemperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(cityTemperature);
+   
+}
+
+function searchCity(city) {
+    let apiKey = "0a8c6e7ct4a98df4436abe8fo0c14f17";
+    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getWeather);
+}
+
+
+
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#weather-app-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
