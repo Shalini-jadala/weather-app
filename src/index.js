2ba0fda486cc0a6dfd5f161b1233b9ee884a7cb6
Shalini-jadala <jspforever143@gmail.com>
2025-11-10 15:00:54 +1100

    injected forecast days from JS

--- src/index.js
+++ src/index.js
@@ -56,8 +56,38 @@ function handleSearchSubmit(event){
     let searchInput = document.querySelector("#search-form-input");
     searchCity(searchInput.value);
 }
+
+function displayForecast() {
+    let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+        forecastHtml = forecastHtml + 
+        ` 
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div> 
+        <div class="weather-forecast-emoji"> ⛅</div> 
+        <div class="weather-forecast-temp">
+            <div class="weather-forecast-temp1">
+            <strong> 15°</strong></div>
+            <div class="weather-forecast-temp1">9°</div> 
+        </div>
+    </div> 
+        `;
+
+    });
+
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml; 
+}
+
+
+
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("India");
+displayForecast();
 
